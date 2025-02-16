5aa231c2e97a3116da780e66e1ed87077280eef6
Isaac Oderberg <isaacoderberg@gmail.com>
2025-02-16 17:12:10 -0500

    created configuration.py

---
+++ puzzle/.ipynb_checkpoints/puzzle_configuration-checkpoint.py
@@ -0,0 +1,22 @@
+## This file contains the coordinates for the puzzle box and each of the puzzle pieces. Puzzle pieces are denoted by letters (A,B,C...etc).
+
+## The pieces with a larger footprint should be labeled first. Then the pieces with the largest amount of that footprint filled in.
+
+A_piece = [
+          (1,0),
+          (1,1),
+          (0,1),
+          (0,2),
+          (x,y),
+          (x,y),
+          (x,y),
+          (x,y),
+          (x,y),
+          (x,y),
+          (x,y),
+          (x,y),
+          (x,y),
+          (x,y),
+          (x,y),
+          (x,y),
+          (x,y),]
